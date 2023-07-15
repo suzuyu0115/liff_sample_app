2da29424b8a030945ae6c6c403d67dd5c3169eb9
suzuyu0115 <szkyut0115@gmail.com>
2023-07-15 15:51:32 +0900

    Add: liffログインの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,17 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    # @current_userがnilでsession[:user_id]に値が入っている場合、ユーザーを持ってくる
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def login_required
+    # ログインしていない（current_userが存在しない場合）root_pathに飛ばす
     redirect_to root_path unless current_user
   end
 
-  def loggewd_in?
+  def logged_in?
     !current_user.nil?
   end
 end
+
